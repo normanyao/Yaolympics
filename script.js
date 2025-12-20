2695e13c1c70e8cf3496797ae4ede00168f7decc
Norman Yao <norman.yao@gmail.com>
2025-12-20 18:19:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -288,6 +288,15 @@ function populatePlayerSelect() {
   });
 }
 
+function shuffleArray(arr) {
+  const a = [...arr];
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
 // ------------------------
 // 5. RENDER YEAR DETAIL
 // ------------------------
