79a68cc63863bccb301c0aedfd1741023424cbd1
Norman Yao <norman.yao@gmail.com>
2025-12-19 20:52:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -105,10 +105,13 @@ const YAOLYMPICS_DATA = {
       displayName: 'Nicolae "The Gypsy" Done',
       nickname: "The Gypsy",
       joinedYear: 2014,
-      hometown: "Example City",
+      hometown: "Buzau, Romania",
       funFact: "Has never missed a Yaolympics.",
       photoUrl: "photos/yaolympians/nicolae_done_yaolympian.png",
-      bio: "Write Nicolae's legendary Yaolympics backstory here. This text will appear under the photo, in a narrower column.",
+      bio: "The fearless leader of The Caravan! A master of watermelon heists, the king of swimming and billiards, and a self-proclaimed Renaissance man,
+         Dr. Done hails from the quaint Romanian village of Buzau. He made his ways to the halls of Harvard College with an intrepid spirit and deep insights into
+        the Diels Alder reaction. After arriving at Harvard, he quickly splurged approximately 100,000 Romanian Leu on a boombox and could be seen proudly walking 
+        around campus with this eyesore on his shoulder.   ",
       yearsAttended: [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
     },
     {
